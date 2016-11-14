451fe4308be47d2dd21ae2fb64d62eb99453f73c
Yury Buldakov <yury.buldakov@gmail.com>
2016-11-15 00:59:35 +0200

    Fix image name for 32bit Slackware

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ VERSIONS="13.37 14.0 14.1 14.2 current"
 for version in ${VERSIONS} ; do
   make VERSION=${version}
   make VERSION=${version} clean
-  docker tag ${user}/slackware-base:${version} ${user}/slackware:${version}
+  docker tag ${user}/slackware-base-32bit:${version} ${user}/slackware-32bit:${version}
 done
-docker tag ${user}/slackware-base:${LATEST} ${user}/slackware:latest
+docker tag ${user}/slackware-base-32bit:${LATEST} ${user}/slackware-32bit:latest
